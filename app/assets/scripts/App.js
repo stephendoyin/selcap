45cface79cfe95b8f19292e2ce43217f6b529a85
stephen <stevedoyin@gmail.com>
2020-08-28 17:09:14 +0100

    initial commit

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,12 @@
+// import "waypoints/lib/noframework.waypoints";
+import NavAnim from "./modules/NavAnimation";
+import MobileNav from "./modules/Mobile-Nav";
+import NavLink from "./modules/Navlink";
+import Faq from "./modules/Faq";
+// import Slider from "./modules/Slider";
+
+let navAnim = new NavAnim();
+let mobileNav = new MobileNav();
+let navLink = new NavLink();
+let faq = new Faq();
+// let slider = new Slider();
